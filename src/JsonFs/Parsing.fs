1c116794bddb089f2b63feec1f9b94b961e913d4
Paul Thomas <paul@codasolutions.co>
2016-06-05 19:55:09 +0100

    fparsec parsers for literal values only

--- src/JsonFs/Parsing.fs
+++ src/JsonFs/Parsing.fs
@@ -47,3 +47,18 @@ module Parsing =
 
     let private valueSeperator =
         pwhitespace .>> skipChar ',' .>> pwhitespace
+
+    (* Values
+       
+       Three literal values that are supported within JSON text, as defined by
+       the RFC 7159 standard.
+
+       For further detailed information, please read:
+           RFC 7159, Section 3; Values
+           See [https://tools.ietf.org/html/rfc7159#section-3] *)
+
+    let private pboolean =
+        stringReturn "true" true <|> stringReturn "false" false .>> pwhitespace
+
+    let private pnull =
+        stringReturn "null" () .>> pwhitespace
