7546f0fae22a13a220091fdbef226d39aeed7193
Paul Thomas <paul@codasolutions.co>
2016-06-05 19:41:37 +0100

    fparsec parsers for parsing basic json grammar

---
+++ src/JsonFs/Parsing.fs
@@ -0,0 +1,49 @@
+﻿namespace JsonFs
+
+module Parsing =
+    open FParsec
+
+    (* Grammar 
+       
+       Common grammatical elements that define the structure within JSON text,
+       as defined by the RFC 7159 standard.
+
+       For further detailed information, please read:
+           RFC 7159, Section 2; JSON Grammar
+           See [https://tools.ietf.org/html/rfc7159#section-2] *)
+
+    [<Literal>]
+    let private space = 0x20
+      
+    [<Literal>]
+    let private horizontalTab = 0x09
+      
+    [<Literal>]
+    let private lineFeed = 0x0A
+      
+    [<Literal>]
+    let private carriageReturn = 0x0D
+
+    let private isWhitespace char = 
+        char = space || char = horizontalTab || char = lineFeed || char = carriageReturn
+
+    let private pwhitespace =
+        skipManySatisfy (int >> isWhitespace)
+
+    let private pbeginArray =
+        pwhitespace .>> skipChar '[' .>> pwhitespace
+
+    let private pbeginObject =
+        pwhitespace .>> skipChar '{' .>> pwhitespace
+
+    let private pendArray =
+        pwhitespace .>> skipChar ']' .>> pwhitespace
+
+    let private pendObject =
+        pwhitespace .>> skipChar '}' .>> pwhitespace
+
+    let private pnameSeperator =
+        pwhitespace .>> skipChar ':' .>> pwhitespace
+
+    let private valueSeperator =
+        pwhitespace .>> skipChar ',' .>> pwhitespace
