372c748fb3958fd482bae759cca9faf35bd8118c
Paul Thomas <paul@codasolutions.co>
2016-06-09 16:44:25 +0100

    created parsers for parsing a basic integer, as defined within the rfc

--- src/JsonFs/Parsers.fs
+++ src/JsonFs/Parsers.fs
@@ -3,6 +3,7 @@
 type Json = 
     | Bool of bool
     | Null of unit
+    | Number of decimal
 
 [<AutoOpen>]
 module Parsers =
@@ -47,14 +48,51 @@ module Parsers =
     let private pnull =
         stringReturn "null" () .>> pwhitespace
 
+    (* Numbers 
+    
+       Numerical representations of numbers that are supported within JSON text,
+       as defined by the RFC 7159 standard.
+       
+       For further detailed information, please read:
+           RFC 7159, Section 6; Number
+           See [https://tools.ietf.org/html/rfc7159#section-6] *)
+
+    [<Literal>]
+    let private zero = 0x30
+    [<Literal>]
+    let private one = 0x31
+    [<Literal>]
+    let private nine = 0x39
+
+    let private digit1to9 i = 
+        i >= one && i <= nine
+
+    let private digit i =
+        digit1to9 i || i = zero
+
+    let private pzero =
+        charReturn '0' "0"
+
+    (* An int is only valid if:
+         a. it has the value 0
+         b. or, it starts with a digit between 1 and 9 and then is followed by n digits *)
+
+    let private pint =
+        pzero <|> (satisfy (int >> digit1to9) .>>. manySatisfy (int >> digit) 
+            |>> fun (first, n) -> string first + n)
+
+    let private pnumber =
+        pint |>> fun i -> decimal i
+
     (* As defined in the FParsec documentation, any recursive parsing needs
        to be forward declared. This will allow parsing of nested JSON elements *)
     
     let internal pjson, pjsonRef = createParserForwardedToRef()
 
     do pjsonRef := choice [ 
-                pboolean |>> Bool
-                pnull    |>> Null
+                pboolean |>> Json.Bool
+                pnull    |>> Json.Null
+                pnumber  |>> Json.Number
             ]
     
     [<RequireQualifiedAccess>]
---
+++ tests/JsonFsTests/NumberTests.fs
@@ -0,0 +1,24 @@
+﻿module NumberTests
+
+open Xunit
+open FsUnit.Xunit
+open JsonFs
+open System
+
+[<Fact>]
+let ``the string "0" is parsed into a decimal value of 0``() =
+    let result = Json.parse "0"
+
+    result |> should equal (Json.Number 0M)
+
+[<Fact>]
+let ``the string "01" is parsed into a decimal value of 0``() =
+    let result = Json.parse "01"
+
+    result |> should equal (Json.Number 0M)
+
+[<Fact>]
+let ``the string "123456789" is parsed into a decimal value of 123456789``() =
+    let result = Json.parse "123456789"
+
+    result |> should equal (Json.Number 123456789M)
