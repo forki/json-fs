04664c2799668471c94a6f63f02c605deb6e3b1c
Paul Thomas <paul@codasolutions.co>
2016-06-28 17:01:53 +0100

    tweaked unit tests as testing conditions shouldn't have been wrapped in escaped quotes

--- tests/JsonFsTests/ArrayTests.fs
+++ tests/JsonFsTests/ArrayTests.fs
@@ -6,18 +6,18 @@ open JsonFs
 
 [<Fact>]
 let ``a empty array is parsed into an empty array``() =
-    let result = Json.parse "\"[]\""
+    let result = Json.parse "[]"
 
     result |> should equal (Json.Array [])
 
 [<Fact>]
 let ``an array containing a single type is parsed into an array``() =
-    let result = Json.parse "\"[1, 2, 3]\""
+    let result = Json.parse "[1, 2, 3]"
 
     result |> should equal (Json.Array [Json.Number 1M; Json.Number 2M; Json.Number 3M])
 
 [<Fact>]
 let ``an array containing multiple types is parsed into an array``() =
-    let result = Json.parse "\"[true, null, 1, \"hello\"]\""
+    let result = Json.parse "[true, null, 1, \"hello\"]"
 
     result |> should equal (Json.Array [Json.Bool true; Json.Null (); Json.Number 1M; Json.String "hello"])
