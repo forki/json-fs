9b53526e98bf3957871938c4b894a81374006d78
Paul Thomas <paul@codasolutions.co>
2016-06-29 06:45:37 +0100

    test support for empty, simple and nested (by a depth of 1) json object structures

---
+++ tests/JsonFsTests/ObjectTests.fs
@@ -0,0 +1,36 @@
+﻿module ObjectTests
+
+open Xunit
+open FsUnit.Xunit
+open JsonFs
+
+[<Fact>]
+let ``an empty object is parsed into an empty map``() =
+    let result = Json.parse "{}"
+
+    result |> should equal (Json.Object Map.empty<string, Json>)
+
+[<Fact>]
+let ``a simple object is parsed into a map with the correct keys and values``() =
+    let result = Json.parse "{\"a\": true, \"b\": null, \"c\": \"hello\", \"d\": 1, \"e\": [1]}"
+
+    let expected = 
+        [("a", Json.Bool true); 
+         ("b", Json.Null ()); 
+         ("c", Json.String "hello"); 
+         ("d", Json.Number 1M);
+         ("e", Json.Array [Json.Number 1M])] |> Map.ofList
+
+    result |> should equal (Json.Object expected)
+
+[<Fact>]
+let ``a nested object is parsed into a nested map with the correct keys and values``() =
+    let result = Json.parse "{\"a\": {\"b\": \"I'm nested\"}}"
+
+    let nested =
+        [("b", Json.String "I'm nested")] |> Map.ofList
+
+    let expected = 
+        [("a", Json.Object nested)] |> Map.ofList
+
+    result |> should equal (Json.Object expected)
