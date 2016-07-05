e927e6a12c4daafb1640d326cf2728905cc8fcca
Paul Thomas <paul@codasolutions.co>
2016-07-05 16:49:28 +0100

    built a very simple performance test that parses 1000 pieces of json using both Newtonsoft.Json and JsonFs. Elapsed time is outputted to the console.

--- tests/JsonFsPerformance/AssemblyInfo.fs
+++ tests/JsonFsPerformance/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace JsonFs.AssemblyInfo
+﻿namespace JsonFsPerformance.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,9 +7,9 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("JsonFs")>]
+[<assembly: AssemblyTitle("JsonFsPerformance")>]
 [<assembly: AssemblyCompany("Coda Solutions Ltd")>]
-[<assembly: AssemblyProduct("JsonFs")>]
+[<assembly: AssemblyProduct("JsonFsPerformance")>]
 [<assembly: AssemblyCopyright("Copyright © 2016")>]
 
 // Version information for an assembly consists of the following four values:
---
+++ tests/JsonFsPerformance/Program.fs
@@ -0,0 +1,35 @@
+﻿open System.Diagnostics
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+open JsonFs
+
+let simpleJson = "{\"FirstName\": \"John\", \"LastName\": \"Doe\", \"DOB\": \"21/03/1984\", \"Age\": 32, \"Occupation\": \"Line Manager\"}"
+
+let parseSimpleJsonWithNewtonsoft =
+    let sw = Stopwatch()
+    sw.Start()
+
+    for i = 1 to 1000 do
+        JObject.Parse(simpleJson) |> ignore
+
+    sw.Stop()
+    printfn "Parsing 1000 simple documents with Newtonsoft took: %O" sw.Elapsed
+
+let parseSimpleJsonWithJsonFs =
+    let sw = Stopwatch()
+    sw.Start()
+
+    for i = 1 to 1000 do
+        Json.parse simpleJson |> ignore
+
+    sw.Stop()
+    printfn "Parsing 1000 simple documents with JsonFs took: %O" sw.Elapsed
+
+[<EntryPoint>]
+let main argv = 
+    
+    // Parse using a simple Json document first
+    parseSimpleJsonWithNewtonsoft
+    parseSimpleJsonWithNewtonsoft
+
+    0
