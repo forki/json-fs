565a682695e12441ec4cddaa687914e376534c18
Paul Thomas <paul@codasolutions.co>
2016-08-25 16:50:30 +0100

    removed redundant json object test

--- tests/JsonFsTests/ObjectTests.fs
+++ tests/JsonFsTests/ObjectTests.fs
@@ -11,12 +11,6 @@ let ``an empty object is parsed into an empty map``() =
     result |> should equal (Object Map.empty<string, Json>)
 
 [<Fact>]
-let ``an empty object is parsed into an empty map when trailed by whitespace``() =
-    let result = Json.parse "{}    "
-
-    result |> should equal (Object Map.empty<string, Json>)
-
-[<Fact>]
 let ``a simple object is parsed into a map with the correct keys and values``() =
     let result = Json.parse "{\"a\": true, \"b\": null, \"c\": \"hello\", \"d\": 1, \"e\": [1]}"
 
